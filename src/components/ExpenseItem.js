8910c252ecf7243f9f1025f32fc4e18db00c55de
Harkirat <hs3188248@gmail.com>
2023-02-10 21:07:43 +0530

    created sub component of expense item i.e expense Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,20 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseData";
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
-function ExpenseItem(props){
-
-    const month = props.date.toLocaleString('en-US',{month:'long'});
-    const day = props.date.toLocaleString('en-US',{day:'2-digit'});
-    const year = props.date.getFullYear();
-
-    return (
-       <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </div>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <h2 className="expense-item__price">{props.amount}</h2>
-        </div>
-       </div>
-    )
+  return (
+    <div className="expense-item">
+      <div>
+        <ExpenseDate  date={props.date} day={day} year={year}/>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2 className="expense-item__price">{props.amount}</h2>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
